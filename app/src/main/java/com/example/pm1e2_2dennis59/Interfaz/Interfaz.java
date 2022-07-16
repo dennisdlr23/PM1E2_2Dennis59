c12123e9beca6ffeef4f113aae810e99a46d772b
dennisdlr23 <dennisdelarosa250@gmail.com>
2022-07-16 00:37:38 -0600

    Prueba1

--- app/src/main/java/com/example/pm1e2_2dennis59/Interfaz/Interfaz.java
+++ app/src/main/java/com/example/pm1e2_2dennis59/Interfaz/Interfaz.java
@@ -1,19 +1,6 @@
 package com.example.pm1e2_2dennis59.Interfaz;
 
-import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-import retrofit2.http.Path;
 public class Interfaz {
-    String Ruta0  = "/posts";
-    String Ruta1 = "/posts/{valor}";
-
-    @GET(Ruta0)
-    public Call<List<Usuarios>> getUsuarios();
-
-    @GET(Ruta1)
-    public Call<Usuarios> getUsuario(@Path("valor") String valor);
 }
 
